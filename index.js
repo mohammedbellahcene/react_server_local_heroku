c33996f2856853ab1fa545565c6ba51d3fc4d857
PierreM <7anUDQ2A3MVJkC7J@gmail.com>
2022-01-05 10:05:09 +0100

    first commit backend

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const app = express();
+
+const data = [
+  {
+    id: 1,
+    artiste: "Les requins du zouglou",
+    musique: "zouglou",
+    zone: "Abidjan",
+    lieu: "L'acoustic, 2 plateaux vallon des jardins",
+    contacts: {
+      tel: "2250505050505",
+      twitter: "@test",
+      "instagram:": "@kjdkj",
+      whatsapp: "2250789387111",
+      facebook: "https://www.facebook.com/LesRequins",
+    },
+    a: 2021,
+    m: 1,
+    d: 10,
+    url: "./assets/img/a.jpg",
+    annule: false,
+  },
+  {
+    id: 2,
+    artiste: "Les requins du zouglou",
+    musique: "zouglou",
+    zone: "Abidjan",
+    lieu: "L'acoustic, 2 plateaux vallon des jardins",
+    contacts: {
+      tel: "2250505050505",
+      twitter: "@test",
+      "instagram:": "@kjdkj",
+      whatsapp: "2250789387111",
+      facebook: "https://www.facebook.com/LesRequins",
+    },
+    a: 2021,
+    m: 1,
+    d: 10,
+    url: "./assets/img/a.jpg",
+    annule: true,
+  },
+  {
+    id: 3,
+    artiste: "Les requins du zouglou",
+    musique: "zouglou",
+    zone: "Abidjan",
+    lieu: "L'acoustic, 2 plateaux vallon des jardins",
+    contacts: {
+      tel: "2250505050505",
+      twitter: "@test",
+      "instagram:": "@kjdkj",
+      whatsapp: "2250789387111",
+      facebook: "https://www.facebook.com/LesRequins",
+    },
+    a: 2021,
+    m: 1,
+    d: 11,
+    url: "./assets/img/a.jpg",
+    annule: false,
+  },
+  {
+    id: 4,
+    artiste: "Les requins du zouglou",
+    musique: "zouglou",
+    zone: "Abidjan",
+    lieu: "L'acoustic, 2 plateaux vallon des jardins",
+    contacts: {
+      tel: "2250505050505",
+      twitter: "@test",
+      "instagram:": "@kjdkj",
+      whatsapp: "2250789387111",
+      facebook: "https://www.facebook.com/LesRequins",
+    },
+    a: 2021,
+    m: 2,
+    d: 1,
+    url: "./assets/img/a.jpg",
+    annule: false,
+  },
+];
+app.get("/", function (req, res) {
+  console.log("appel");
+  res.json(data);
+});
+
+app.listen(3010);
